a67c0d9dff7e8704d1a6eb8290a888a0b14057d8
Pranavamurthy KS <pranavamurthyks45@gmail.com>
2026-06-13 17:58:52 +0530

    Delete backend directory

--- backend/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-async function connectDatabase() {
-    const mongoUri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/orbit';
-    await mongoose.connect(mongoUri);
-    console.log('MongoDB connected');
-}
-
-module.exports = connectDatabase;
